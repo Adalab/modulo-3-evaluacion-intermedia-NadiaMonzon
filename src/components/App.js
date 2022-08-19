1e8ffbdce0a9601db3f82a8f90edc3e28b0af13a
NadiaMonzon <nadiamonque99@gmail.com>
2022-08-19 08:35:04 +0100

    Change some html, add add functionality

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,10 @@ function App() {
 
   //variables de estado
   const [quote, setQuote] =useState(quotes)
+  const [newQuote, setNewQuote] =useState({
+    quote: "",
+    character:"",
+  })
 
   //renderizado
 
@@ -16,6 +20,19 @@ function App() {
     </div>)
   })
 
+  const handleNewQuote= (ev) =>{
+    setNewQuote({
+      ...newQuote, 
+      [ev.target.id]: ev.target.value,
+    })
+
+  }
+
+  const handleAddQuote = (ev) =>{
+    ev.preventDefault();
+    setQuote([...quote, newQuote])
+  }
+
   //filtrado
 
 
@@ -25,22 +42,31 @@ function App() {
     <div className="App">
       <main>
         <h1>Frases de Friends</h1>
-        <label htmlFor="">Filtrar por frase</label>
-        <input type="text"/>
-
-        <label htmlFor="">Filtrar por personaje</label>
-        <select>
-          <option value="Todos">Todos</option>
-          <option value="Ross">Ross</option>
-          <option value="Monica">Monica</option>
-          <option value="Joey">Joey</option>
-          <option value="Phoebe">Phoebe</option>
-          <option value="Chandler">Chandler</option>
-          <option value="Rachel">Rachel</option>
-        </select>
+        <fieldset>
+          <label htmlFor="">Filtrar por frase</label>
+          <input type="text"/>
+          <label htmlFor="">Filtrar por personaje</label>
+          <select>
+            <option id="Todos" value="Todos">Todos</option>
+            <option id="Ross" value="Ross">Ross</option>
+            <option id="Monica" value="Monica">Monica</option>
+            <option id="Joey" value="Joey">Joey</option>
+            <option id="Phoebe" value="Phoebe">Phoebe</option>
+            <option id="Chandler" value="Chandler">Chandler</option>
+            <option id="Rachel" value="Rachel">Rachel</option>
+          </select>
+        </fieldset>
 
         {quoteList}
 
+        <fieldset>
+          <h2>Añadir una nueva frase</h2>
+          <label htmlFor="">Frase</label>
+          <input type="text" id='quote' value={newQuote.quote} onChange={handleNewQuote}/>
+          <label htmlFor="">Personaje</label>
+          <input type="text" id='character' value={newQuote.character} onChange={handleNewQuote}/>
+          <button onClick={handleAddQuote}>Añadir una nueva frase</button>
+        </fieldset>
 
       </main>
     </div>
