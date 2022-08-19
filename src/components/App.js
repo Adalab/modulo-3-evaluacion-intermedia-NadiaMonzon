1f8279e54b20daa8174296eba8dd49b85a729a42
NadiaMonzon <nadiamonque99@gmail.com>
2022-08-19 08:01:19 +0100

    Added initial html and json

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,30 @@
-import logo from '../images/logo.svg';
 import '../styles/App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <h1>Frases de Friends</h1>
+        <label htmlFor="">Filtrar por frase</label>
+        <input type="text"/>
+
+        <label htmlFor="">Filtrar por personaje</label>
+        <select>
+          <option value="Todos">Todos</option>
+          <option value="Ross">Ross</option>
+          <option value="Monica">Monica</option>
+          <option value="Joey">Joey</option>
+          <option value="Phoebe">Phoebe</option>
+          <option value="Chandler">Chandler</option>
+          <option value="Rachel">Rachel</option>
+        </select>
+
+        <div>
+          <p>Pivot!Pivot!Pivot!PivotPivot <span>-Ross</span></p>
+        </div>
+
+
+      </main>
     </div>
   );
 }
